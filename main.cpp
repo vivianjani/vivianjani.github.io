5e897176a65056c53c8a566d34cadd06af8cc2b3
Septi Vivi Anjani <106797559+vivianjani@users.noreply.github.com>
2023-03-15 23:13:12 +0700

    algoritma_quck_sort

--- main.cpp
+++ main.cpp
@@ -2,16 +2,51 @@
 
 using namespace std;
 
-int main()
-{
-    float luas, phi=3.14;
-    int r;
+void quick(int arr[],int left, int right){
 
-    cout << "Masukkan Jari-jari : ";
-    cin >> r;
+    int i= left, j= right;
+    int tmp;
+    int pivot= arr[(left+right/2)];
+    while (i<j){
+        while (arr[i] < pivot)
+            i++;
+            while (arr[j]> pivot)
+            j--;
 
-    luas = phi*r*r;
-
-    cout<<"Luas Lingkaran = "<<luas<< endl;
-    return 0;
+    if(i<= j){
+        tmp = arr[i];
+        arr[i]= arr[j];
+        arr[j]= tmp;
+        i++;
+        j--;
+    };
 }
+if (left <j){
+        quick(arr,left,j);
+if (i< right){
+    quick(arr,i,right);
+}
+}
+}
+int main(){
+    int n, data[50];
+
+    cout<<"Masukkan Banyak Angka : ";
+    cin>>n;
+
+    for (int a=0; a<n; a++){
+        cout<<"Masukkan Angka : ";
+        cin>>data[a];
+    }
+     cout<<"Data Sebelum diurutkan : ";
+        for (int a=0; a<n; a++){
+            cout<<data[a]<<" ";
+        }
+        quick(data,0,n);
+        cout<<"\n";
+        cout<<"Hasil Pengurutan : ";
+        for (int a=0; a<n; a++){
+            cout<<data[a]<<" ";
+        }
+}
+
